5f6a8d870c42854774655cdb7e97b8c45fb6676e
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2018-07-01 11:35:28 -0500

    Tweaking app.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import regeneratorRuntime from "regenerator-runtime";
 import Router from "preact-router";
 import AsyncRoute from "preact-async-route";
 import { h, render, Component } from "preact";
 import Search from "./components/Search/Search";
 
-const mainElement = document.getElementsByTagName("main")[0];
-
-const Main = () => (
-  <Router>
-    <Search path="/" default/>
-    <AsyncRoute path="/pedal/:id" getComponent={() => import(/* webpackChunkName: "PedalDetail" */ "./components/PedalDetail/PedalDetail").then(module => module.default)}/>
-    <AsyncRoute path="/favorites" getComponent={() => import(/* webpackChunkName: "Favorites" */ "./components/Favorites/Favorites").then(module => module.default)}/>
-  </Router>
-);
-
-render(<Main/>, mainElement, mainElement.lastChild);
+render(<Router>
+  <Search path="/" default/>
+  <AsyncRoute path="/pedal/:id" getComponent={() => import(/* webpackChunkName: "PedalDetail" */ "./components/PedalDetail/PedalDetail").then(module => module.default)}/>
+  <AsyncRoute path="/favorites" getComponent={() => import(/* webpackChunkName: "Favorites" */ "./components/Favorites/Favorites").then(module => module.default)}/>
+</Router>, document.getElementById("app"));
