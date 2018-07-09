b080b577590f05b839eb8d7cf45ea1aff85c07f7
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2018-07-09 09:32:57 -0500

    Fixing URI issue in PedalImage component.

--- src/components/PedalImage/PedalImage.js
+++ src/components/PedalImage/PedalImage.js
@@ -2,8 +2,8 @@ import { h, render } from "preact";
 
 export default (props) => (
   <picture>
-    <source srcset={props.id === null ? "" : `/images/${props.id}-2x.webp 2x, /images/pedals/${props.id}-1x.webp 1x`} type="image/webp"/>
-    <source srcset={props.id === null ? "" : `/images/${props.id}-2x.jpg 2x, /images/pedals/${props.id}-1x.jpg 1x`} type="image/jpeg"/>
+    <source srcset={props.id === null ? "" : `/images/${props.id}-2x.webp 2x, /images/${props.id}-1x.webp 1x`} type="image/webp"/>
+    <source srcset={props.id === null ? "" : `/images/${props.id}-2x.jpg 2x, /images/${props.id}-1x.jpg 1x`} type="image/jpeg"/>
     <img src={props.id === null ? "" : `/images/${props.id}-1x.jpg`} alt={`${props.manufacturer} ${props.model}`} />
   </picture>
 );
