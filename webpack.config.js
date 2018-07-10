f9c816724a6358c105df0c0902b21ae16c315e40
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2018-07-10 10:54:10 -0500

    Adjust config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,11 @@ const path = require("path");
 const webpack = require("webpack");;
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const devMode = process.env.NODE_ENV !== "production";
 const paths = {
   src: path.join(__dirname, "src"),
   dist: path.join(__dirname, "dist")
 };
-
-let plugins = [
-  new CleanWebpackPlugin(paths.dist),
-  new HtmlWebpackPlugin({
-    template: path.join(paths.src, "html", "app.html"),
-    filename: path.join(paths.dist, "index.html"),
-    inject: true,
-    hash: false,
-    minify: {
-      removeComments: devMode ? false : true,
-      collapseWhitespace: devMode ? false : true,
-      minifyJS: devMode ? false : true,
-      minifyCSS: devMode ? false : true
-    }
-  })
-];
 
 module.exports = {
   mode: devMode ? "development" : "production",
@@ -40,9 +23,12 @@ module.exports = {
           chunks: "all"
         }
       }
+    },
+    runtimeChunk: {
+        name: "vendors"
     }
   },
-  devtool: devMode ? "inline-source-map" : "none",
+  devtool: "source-map",
   output: {
     filename: devMode ? "js/[name].js" : "js/[name].[chunkhash:8].js",
     path: paths.dist,
@@ -59,5 +45,19 @@ module.exports = {
       }
     ]
   },
-  plugins: plugins
+  plugins: [
+    new CleanWebpackPlugin(paths.dist),
+    new HtmlWebpackPlugin({
+      template: path.join(paths.src, "html", "app.html"),
+      filename: path.join(paths.dist, "index.html"),
+      inject: true,
+      hash: false,
+      minify: {
+        removeComments: devMode ? false : true,
+        collapseWhitespace: devMode ? false : true,
+        minifyJS: devMode ? false : true,
+        minifyCSS: devMode ? false : true
+      }
+    })
+  ]
 };
